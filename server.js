9b932ff13c0e0424c805473024813f5f58e8f0b8
declee <derricklee6@gmail.com>
2020-10-07 01:37:02 -0700

    heroku deploy changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dnp-reddit-fe'));
+app.use(express.static(__dirname + '/dist/dnp-reddit-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/dnp-reddit-fe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dnp-reddit-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
