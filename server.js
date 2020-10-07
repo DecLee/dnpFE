beb75f9dc3db3ecc96fa5f83a78bd190252af8cb
declee <derricklee6@gmail.com>
2020-10-07 14:58:54 -0700

    minor changes to server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dnpredditfe'));
+app.use(express.static(__dirname + '/dist/dnpRedditFE'));
 
 app.get('/*', function(req,res) {
     
 console.log(__dirname);
-res.sendFile(path.join(__dirname+'/dist/dnpredditfe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dnpRedditFE/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
